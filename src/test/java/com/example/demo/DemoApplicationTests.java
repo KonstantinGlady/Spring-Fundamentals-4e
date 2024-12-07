9e115b587c4dc30e10323944519623e3a56552c9
Glady <gladyi@hotmail.com>
2024-12-07 04:12:45 +0300

    Spring application context

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context);
+        int count = context.getBeanDefinitionCount();
+        System.out.println("Number of beans " + count);
+        for (var v : context.getBeanDefinitionNames()) {
+            System.out.println(v);
+        }
+    }
 
 }
